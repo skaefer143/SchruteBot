a1500e8358926850c49cfba38f1f36d96d0edc46
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2016-11-09 17:46:40 -0700

    made implementation plan is GhostManager

--- UAlbertaBot/Source/GhostManager.cpp
+++ UAlbertaBot/Source/GhostManager.cpp
@@ -6,3 +6,25 @@ using namespace UAlbertaBot;
 GhostManager::GhostManager() 
 { 
 }
+
+/*
+
+gameplan: use informationManager to find the base location. once the base location is known, move the ghost to that location
+keep ghost cloaked while moving through base. Once at base, hide behind resources. Once nuke is ready, cloak, find command centre/nexus/hatchery
+ (refered to as Nexus for simplicity) and send a nuke near it. once nuke is on the way, move the ghost away. 
+
+ if sneaking through base is imposiible, then use ghost to nuke defenses
+
+*/
+
+/*
+
+implentation:
+-find base location
+-move to enemy base
+-find Nexus
+-know if there is a nuke available
+-sneak through base
+-avoid detectors
+
+*/
--- UAlbertaBot/Source/GhostManager.h
+++ UAlbertaBot/Source/GhostManager.h
@@ -11,5 +11,10 @@ public:
 
 	GhostManager();
 	void executeMicro(const BWAPI::Unitset & targets);
+
+	int getAttackPriority(BWAPI::Unit rangedUnit, BWAPI::Unit target);
+	BWAPI::Unit getTarget(BWAPI::Unit rangedUnit, const BWAPI::Unitset & targets);
+
+	bool shouldNuke();
 };
 }
