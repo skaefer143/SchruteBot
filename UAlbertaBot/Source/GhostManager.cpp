0a5ea710e42d1a44b99345792493b64c31419d83
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2016-12-01 18:29:15 -0700

    started working on ghost cloaking and lockdown abilities

--- UAlbertaBot/Source/GhostManager.cpp
+++ UAlbertaBot/Source/GhostManager.cpp
@@ -5,26 +5,76 @@ using namespace UAlbertaBot;
 
 GhostManager::GhostManager() 
 { 
+	
 }
 
+void GhostManager::executeMicro(const BWAPI::Unitset & targets)
+{
+	const BWAPI::Unitset & ghosts = getUnits();
+
+	// figure out targets
+	BWAPI::Unitset ghostTargets;
+	std::copy_if(targets.begin(), targets.end(), std::inserter(ghostTargets, ghostTargets.end()),
+		[](BWAPI::Unit u){ return u->isVisible();  });
+
+	//set the range of the ghost
+	int ghostRange = BWAPI::UnitTypes::Terran_Ghost.groundWeapon().maxRange() - 16;
+	//set energy requirements
+	int cloakingEnergy = BWAPI::UnitTypes::Terran_Ghost.cloakingTech().energyCost;
+	int lockdownEnergy = BWAPI::TechTypes::Lockdown.energyCost();
+
+
+	for (auto & ghost : ghosts)
+	{
+		bool ghostNearEnemy = false;
+		if (order.getType() == SquadOrderTypes::Attack)
+		{
+			// if attacking, do not attack units
+
+			// find the best target for this tank
+			BWAPI::Unit target = getTarget(ghost, ghostTargets);
+			if (!targets.empty()){
+
+				
+				// cloak
+				if (ghost->getDistance(target) < 2 * ghostRange && !ghost->isCloaked && ghost->canCloak() && ghost->getEnergy > 1.5*cloakingEnergy){
+					ghostNearEnemy = true;
+					ghost->cloak();
+				}
+
+				//lockdown enemy carriers
+				if (target->getType == BWAPI::UnitTypes::Protoss_Carrier && !target->isLockedDown() && ghost->getEnergy > lockdownEnergy){
+					//maybe check if the tech has been researched
+					ghost->useTech(BWAPI::TechTypes::Lockdown, target);
+				}
+
+			}
+		}
+		else if (order.getType() == SquadOrderTypes::Defend)
+		{
+			// find the best target for this tank
+			BWAPI::Unit target = getTarget(ghost, ghostTargets);
+		}
+	}
+}
+
+
+
+
+
 /*
 
-gameplan: use informationManager to find the base location. once the base location is known, move the ghost to that location
-keep ghost cloaked while moving through base. Once at base, hide behind resources. Once nuke is ready, cloak, find command centre/nexus/hatchery
- (refered to as Nexus for simplicity) and send a nuke near it. once nuke is on the way, move the ghost away. 
-
- if sneaking through base is imposiible, then use ghost to nuke defenses
-
-*/
-
-/*
-
-implentation:
--find base location
+nuking implentation:
+-wait until nuke is ready
 -move to enemy base
--find Nexus
--know if there is a nuke available
--sneak through base
--avoid detectors
+-cloke if enemy is in range
+-find building target
+-launch nuke
+
+anti-carrier implementation
+-find carrier target
+-lockdown
+-shoot carrier
+-refresh lockdown if it's not dead. 
 
 */
--- UAlbertaBot/Source/GhostManager.h
+++ UAlbertaBot/Source/GhostManager.h
@@ -7,10 +7,14 @@ namespace UAlbertaBot
 {
 class GhostManager : public MicroManager
 {
+
 public:
 
 	GhostManager();
 	void executeMicro(const BWAPI::Unitset & targets);
+
+	BWAPI::Unit chooseTarget(BWAPI::Unit rangedUnit, const BWAPI::Unitset & targets, std::map<BWAPI::Unit, int> & numTargeting);
+	BWAPI::Unit closestrangedUnit(BWAPI::Unit target, std::set<BWAPI::Unit> & rangedUnitsToAssign);
 
 	int getAttackPriority(BWAPI::Unit rangedUnit, BWAPI::Unit target);
 	BWAPI::Unit getTarget(BWAPI::Unit rangedUnit, const BWAPI::Unitset & targets);
