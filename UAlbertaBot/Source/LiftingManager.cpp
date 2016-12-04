aabfacef91b831cf7f8b1358751f06b1334f2f54
skaefer143 <stormkaefer@gmail.com>
2016-12-04 00:58:49 -0700

    cleaned up the update function. Still has weird bug where buildings won't land though. Doesn't seem to be my fault though... dangit bwapi

--- UAlbertaBot/Source/LiftingManager.cpp
+++ UAlbertaBot/Source/LiftingManager.cpp
@@ -12,8 +12,6 @@ LiftingManager & LiftingManager::Instance()
 void LiftingManager::update(){
 	//check if part of the wall needs to be lifted, to let troops through.
 	if (ProductionManager::Instance().getMadeFirstWall()){
-		LiftingManager liftMan = LiftingManager::Instance();
-		UnitData myUnitData = InformationManager::Instance().getUnitData(BWAPI::Broodwar->self());
 		std::vector<Building> myBuildings = BuildingManager::Instance().buildingsWeHaveBuilt;
 		for (Building & b : myBuildings){
 			if (b.isPartOfWall &&
@@ -22,12 +20,12 @@ void LiftingManager::update(){
 				//checks if building is a Barracks or a Factory, only two buildings in wall we are potentially working with
 
 				if (b.buildingUnit->isLifted()){
-					liftMan.checkForSetDown(b);
+					checkForSetDown(b);
 				}
 				else if (b.buildingUnit->isCompleted() && b.isPartOfWall && !b.buildingUnit->isLifted()
 					&& b.buildingUnit->getType().getRace() == BWAPI::Races::Terran){
 					//lift wall, in lifting manager
-					liftMan.checkForLiftOff(b);
+					checkForLiftOff(b);
 				}
 			}
 		}
@@ -73,7 +71,7 @@ bool LiftingManager::enemyUnitsNear(Building b){
 }
 
 bool LiftingManager::myUnitsNear(Building b){
-	Unitset nearbyUnits = b.buildingUnit->getUnitsInRadius(50, Filter::IsOwned);
+	Unitset nearbyUnits = b.buildingUnit->getUnitsInRadius(100, Filter::IsOwned);
 	for (auto unit : nearbyUnits){
 		if (unit->isMoving() && unit->canMove()){
 			//check if unit is moving, so it's not a building
